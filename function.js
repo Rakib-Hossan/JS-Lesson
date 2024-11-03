e03fe220e794d9f9a599ad8df24e7b943ae68885
Rakib-Hossan <rakib.redoy01@gmail.com>
2024-11-03 09:51:37 +0600

    learn overview of js function

---
+++ function.js
@@ -0,0 +1,15 @@
+/************
+ JS Function
+ **********/
+
+/*
+    A JavaScript function is a block of code designed to perform a particular task.
+    A JavaScript function is executed when 'something' invokes it.
+ */
+
+// Example of JS Function
+function country() {
+  console.log("My country name is Bangladesh");
+}
+
+country(); //invoke or call funtion.
